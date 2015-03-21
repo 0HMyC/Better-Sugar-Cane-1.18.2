7e1dcc1e3d8399074f3b62e863f5c3ba9ae2ab21
Numbah90 <numbah90@yahoo.com>
2015-03-21 17:39:15 -0400

    Initial files

---
+++ src/main/java/com/numbah90/bettersugarcane/BetterSugarCane.java
@@ -0,0 +1,30 @@
+package com.numbah90.bettersugarcane;
+
+import com.numbah90.bettersugarcane.help.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class BetterSugarCane
+{
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/numbah90/bettersugarcane/help/Reference.java
@@ -0,0 +1,7 @@
+package com.numbah90.bettersugarcane.help;
+
+public class Reference
+{
+    public static final String MODID = "bettersugarcane";
+    public static final String VERSION = "1.0";
+}
