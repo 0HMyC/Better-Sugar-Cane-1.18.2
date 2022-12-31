ea80d789abb3bb78211f0df4cd14b39a931f7e40
0HMyC <65702872+0HMyC@users.noreply.github.com>
2022-12-31 02:11:57 -0800

    Delete src/main/java/com/numbah90/bettersugarcane/help directory

--- src/main/java/com/numbah90/bettersugarcane/help/Reference.java
+++
@@ -1,7 +0,0 @@
-package com.numbah90.bettersugarcane.help;
-
-public class Reference
-{
-    public static final String MODID = "bettersugarcane";
-    public static final String VERSION = "2.1";
-}
